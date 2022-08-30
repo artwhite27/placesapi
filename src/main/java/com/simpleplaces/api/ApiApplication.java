ebcf00a0bb25373fbeb3b171c6227fbfcafbc591
Artavazd Vardanyan <artavazdvardanyan27@gmail.com>
2022-08-30 13:50:52 +0400

    Enable swagger

--- src/main/java/com/simpleplaces/api/ApiApplication.java
+++ src/main/java/com/simpleplaces/api/ApiApplication.java
@@ -1,19 +1,11 @@
 package com.simpleplaces.api;
 
-import com.google.maps.FindPlaceFromTextRequest;
-import com.google.maps.GeoApiContext;
-import com.google.maps.PlacesApi;
-import com.google.maps.errors.ApiException;
-import com.google.maps.model.FindPlaceFromText;
-import com.google.maps.model.PlaceDetails;
-import com.simpleplaces.api.config.GoogleMapsConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-import java.io.IOException;
-import java.util.Arrays;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
+@EnableSwagger2
 public class ApiApplication {
 
     public static void main(String[] args) {
